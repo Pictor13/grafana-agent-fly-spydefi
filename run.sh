a6eb3d695efa7d49b052d49761692c3e9144fc09
Igor Pellegrini <igor.pellegrini@live.com>
2023-09-05 15:15:27 +0200

    Fixes for Spydefi monitoring

--- run.sh
+++ run.sh
@@ -4,3 +4,5 @@ exec /bin/grafana-agent                                     \
   --config.file=/etc/agent/agent.yaml                       \
   --metrics.wal-directory=/etc/agent/data                   \
   --config.expand-env                                       \
+  --server.http.address 0.0.0.0:12345                       \
+  --server.grpc.address 0.0.0.0:12346
--- set_secrets.sh
+++ set_secrets.sh
@@ -1,21 +1,21 @@
 echo "Setting required secrets at fly.io..."
 
 # "Supabase service role secret (https://supabase.com/dashboard/project/_/settings/api)"
-flyctl secrets set SUPABASESERVICEROLESECRET="value"
+flyctl secrets set SUPABASESERVICEROLESECRET="$SUPABASE_SERVICE_ROLE_JWT"
 
 # "Supabase project url (https://supabase.com/dashboard/project/_/settings/api)"
 # "(exclude the https:// part, example.: zzzzzzzzzzzzzzzzzzzz.supabase.co)"
-flyctl secrets set SUPABASEPROJECTURL="value"
+flyctl secrets set SUPABASEPROJECTURL="$SUPABASE_PROJECT_ID.supabase.co"
 
 # "Grafana prometheus remote write endpoint"
 # "grafana cloud / my account / prometheus details / remote write endpoint"
-flyctl secrets set REMOTEWRITEENDPOINT="value"
+flyctl secrets set REMOTEWRITEENDPOINT="$GRAFANA_REMOTE_WRITE_ENDPOINT"
 
 # "Grafana prometheus Username"
 # "grafana cloud / my account / prometheus details / (Username / Instance ID)"
-flyctl secrets set REMOTEUSERNAME="value"
+flyctl secrets set REMOTEUSERNAME="$GRAFANA_API_INSTANCEID"
 
 # "Grafana prometheus remote write password"
 # "grafana cloud / my account / prometheus details / (Password / API Key)"
-flyctl secrets set REMOTEWRITEPASSWORD="value"
+flyctl secrets set REMOTEWRITEPASSWORD="$GRAFANA_API_TOKEN"
 
